b285e949559a9e1dde226f0ada7717732aa85e63
ViniciusGarciaSilva <vinicius.garcia.silva@usp.br>
2018-12-13 22:45:53 -0200

    Updates

---
+++ precache-manifest.aeac3ad1981cafc4101f9ac74087bce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gg-izi-serv/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "91d52d20d37abb38d385",
+    "url": "/gg-izi-serv/static/js/runtime~main.91d52d20.js"
+  },
+  {
+    "revision": "52c32d0792d9408c779c",
+    "url": "/gg-izi-serv/static/js/main.52c32d07.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/gg-izi-serv/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "52c32d0792d9408c779c",
+    "url": "/gg-izi-serv/static/css/main.6a23887f.chunk.css"
+  },
+  {
+    "revision": "645e764e3fde2e700948cdd5d134da1f",
+    "url": "/gg-izi-serv/index.html"
+  }
+];
